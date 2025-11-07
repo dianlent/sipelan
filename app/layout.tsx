c7d6c7ef483f413466bd1b0ebb8a440107861908
dianlent <71243976+dianlent@users.noreply.github.com>
2025-11-07 14:28:31 +0700

    Import SpeedInsights from Vercel in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from '@/contexts/AuthContext'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const poppins = Poppins({ 
   weight: ['300', '400', '500', '600', '700', '800'],
   subsets: ['latin'],
